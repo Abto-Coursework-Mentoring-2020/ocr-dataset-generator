43b4295533124ab033be8ffaa67a98b68f09e8f3
oksayar <oksyarysh@gmail.com>
2020-11-05 16:36:21 +0200

    Fixed main and reconstructed it to fit the whole system

--- generate.py
+++ generate.py
@@ -2,26 +2,34 @@ import os
 import argparse
 from src.clear_text_image_generator import generate_clear_text_images
 from src.downscaled_image_generator import generate_downscaled_images
-
+from src.downscaled_image_generator import blur_images
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # dataset type (one of 'text', 'downscale', 'blur')
-    parser.add_argument('-t', '--target', required=True) 
-    # output directory 
-    parser.add_argument('-o', '--out-dir', default='./tmp') 
-    
+    parser.add_argument('-t', '--target', required=True)
+    # output directory
+    parser.add_argument('-o', '--out-dir', default='./tmp')
+
     # PARAMETERS FOR CLEAR TEXT IMAGES GENERATION
-    # path of the file with text for clear text images generation  
+    # path of the file with text for clear text images generation
     parser.add_argument('-tfp', '--text-file-path')
-    
+
     # PARAMETERS FOR DOWNSCALED IMAGES GENERATION
-    # path of the file with text for clear text images generation  
+    # path of the file with text for clear text images generation
     parser.add_argument('-id', '--images-dir')
     parser.add_argument('-hg', '--height')
     parser.add_argument('-wd', '--width')
     parser.add_argument('-i', '--interpolation')
-    
+
+    # PARAMETERS FOR BLURRING IMAGES
+    # path of the dir with images
+    parser.add_argument('-id', '--images-dir')
+    parser.add_argument('-fn', '--filename')
+    # filter for blurring - can be gaussian, box, min, max or median
+    parser.add_argument('-fl', '--filter')
+    parser.add_argument('-r', '--radius')
+
     args = parser.parse_args()
 
     if args.target == 'text':
@@ -32,7 +40,7 @@ if __name__ == '__main__':
 
     elif args.target == 'blur':
         # your blur dataset generation function here
-        pass
+        blur_images(args.images_dir, args.filename, args.filter, int(args.radius))
     else:
         raise ValueError('Ivalid value for target option specified.')
 
